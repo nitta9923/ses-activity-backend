cddae0b3fee3b778bc0d6935be673da60157164d
nitta9923 <puudai.0509@gmail.com>
2025-04-27 18:40:59 +0900

    Docker構築

---
+++ src/main/java/com/hogehoge/ses/activity/ses_activity_backend/SesActivityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hogehoge.ses.activity.ses_activity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesActivityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesActivityBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hogehoge/ses/activity/ses_activity_backend/SesActivityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogehoge.ses.activity.ses_activity_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesActivityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
